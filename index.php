987a9c3c14642be8abfce79de3f3bc130b230468
Lunar-Design <lunar.appsite@gmail.com>
2017-04-23 12:38:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 $link = 'https://drive.google.com/file/d/0B6VYU2mjTdy0WVRjb1BJUU1hYXM/view';
 $api = 'http://api.getlinkdrive.com/getlink?url='.$link;
-$curl = curl($api);
-$data = json_decode($curl);
-echo "<pre>";
-print_r($data);
-echo "</pre>";
+$sources = curl($api);
 function curl($url)
 {
 	$ch = @curl_init();
@@ -14,11 +10,7 @@ function curl($url)
 	$head[] = "Keep-Alive: 300";
 	$head[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
 	$head[] = "Accept-Language: en-us,en;q=0.5";
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; 
-
-WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 
-
-Safari/537.36');
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36');
 	curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -32,3 +24,30 @@ Safari/537.36');
 	return $page;
 }
 ?>
+<script type="text/javascript" src="http://api.getlinkdrive.com/js/videojs/video.js"></script>
+<script type="text/javascript" src="http://api.getlinkdrive.com/js/videojs/videojs-resolution-switcher.js"></script>
+<link href="http://api.getlinkdrive.com/js/videojs/video-js.css" rel="stylesheet">
+<link href="http://api.getlinkdrive.com/js/videojs/videojs-resolution-switcher.css" rel="stylesheet">
+<div id="player" style="width: 100%;height: 100%">
+<video id="video" class="video-js vjs-default-skin"></video>
+</div>
+<script type="text/javascript">
+videojs('video', {
+    width: 'auto',
+    height: 'auto',
+    controls: true,
+    plugins: {
+      videoJsResolutionSwitcher: {
+        default: 480,
+        dynamicLabel: true
+      }
+    }
+}, function(){
+    var player = this;
+    window.player = player
+    player.updateSrc(<?php echo $sources; ?>)
+    player.on('resolutionchange', function(){
+      	console.info('Source changed to %s', player.src())
+    })
+});
+</script>
